4d9b30eb208191582f20b596484d94355e23503c
Neptunya <neptunyamai@gmail.com>
2024-11-22 17:04:40 -0800

    added mui multiple select menus for filters

---
+++ src/components/MultipleSelectChipFilters.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import { ThemeProvider } from '@mui/material/styles'; // Import ThemeProvider
+import CssBaseline from '@mui/material/CssBaseline'; // For global Material-UI styles
+import { theme } from '../styles/theme';
+import MultipleSelectChipContent from './MutipleSelectChipContent'
+
+export default function MultipleSelectChipFilters() {
+    const [isClient, setIsClient] = useState(false);
+    
+    const rarities = [
+      '✧✧',
+      '✧✧✧',
+      '✧✧✧✧',
+      '✧✧✧✧✧',
+    ];
+    
+    const slots = [
+      'Hair','Dress','Top','Bottom','Socks','Shoes',
+      'Hair Accessory','Headwear','Earrings','Neckwear','Bracelet','Choker','Gloves',
+      'Face Decoration','Chest Accessory','Pendant','Backpiece','Ring','Handheld',
+      'Base Makeup','Eyebrows','Eyelashes','Contact Lenses','Lips'
+    ];
+    
+    const sources = [
+      'F2P',
+      'RNG',
+      'Gacha',
+    ];
+    
+    const styles = [
+      'Elegant',
+      'Fresh',
+      'Sweet',
+      'Sexy',
+      'Cool'
+    ];
+    
+    const labels = [
+      'Warm','Summer','Home','Formal','Simple',
+      'Fantasy','Intellectual','Adventure','Romance','Retro',
+      'Fashion','Uniform','Playful','Trendy','Fairy',
+      'Ballroom','Royal','Linlang','Pastoral','Cute'
+    ]
+    
+    useEffect(() => {
+        setIsClient(true);
+      }, []);
+    return isClient ? (
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> {}
+      <MultipleSelectChipContent names={rarities} idPrefix="rarity" label="Rarity" />
+      <MultipleSelectChipContent names={slots} idPrefix="slot" label="Type" />
+      <MultipleSelectChipContent names={sources} idPrefix="source" label="Source" />
+      <MultipleSelectChipContent names={styles} idPrefix="style" label="Style" />
+      <MultipleSelectChipContent names={labels} idPrefix="label" label="Label" />
+    </ThemeProvider>
+  ) : null;
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,24 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: { main: '#edb1bd' },
+    secondary: { main: '#b38aca' },
+    background: {
+      default: '#292535',
+      paper: '#353142',
+    },
+    text: {
+      primary: '#ffffff',
+      secondary: '#ffffff',
+      disabled: 'rgba(255,255,255,0.25)',
+      hint: '#ffffff',
+    },
+    info: { main: '#a3c7f2' },
+    divider: '#edb1bd',
+  },
+  typography: {
+    fontFamily: 'PT Sans',
+  },
+});
